d766cbe2333e9504ec7b6193b312f74830182e54
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-02-24 15:13:22 -0500

    create routes to retrieve database values

--- server.js
+++ server.js
@@ -18,6 +18,44 @@ const db = new sqlite3.Database('./db/election.db', err => {
   
     console.log('Connected to the election database.');
 });
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+// returns an array of objects for each row of the table in the database
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
 //test server.js connection
 // app.get('/', (req, res) => {
 //     res.json({
