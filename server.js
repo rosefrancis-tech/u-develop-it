1ee482dfddf38205fa7ad91f9f7d473fcaec73a3
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-02-24 12:08:31 -0500

    connect to database

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Import express
 const express = require('express');
+// Import SQLite
+const sqlite3 = require('sqlite3').verbose();
+
 // PORT Designation
 const PORT = process.env.PORT || 3001;
 // app expression
@@ -7,6 +10,14 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+});
 //test server.js connection
 // app.get('/', (req, res) => {
 //     res.json({
@@ -20,6 +31,9 @@ app.use((req, res) => {
   });
 
 // Start Express.js server on port 3001
+// Start server after DB connection
+db.on('open', () => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+});
